408c0dfc7a80273a65ce1ce1b84f0648d6bbc9fb
Yuliya Pavlenko <yuliapavlenko05@gmail.com>
2018-09-01 19:11:36 +0300

    init commit

---
+++ restaraunt-service/src/main/java/com/restaraunt/RestarauntApplication.java
@@ -0,0 +1,12 @@
+package com.restaraunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestarauntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestarauntApplication.class, args);
+	}
+}
