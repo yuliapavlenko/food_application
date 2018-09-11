ab2d0d3126aafd9ffa815951e4a209bd9b47b7a5
Yuliya Pavlenko <yuliapavlenko05@gmail.com>
2018-09-12 02:50:53 +0300

    added exception handler for services

--- restaraunt-service/src/main/java/com/restaraunt/dao/services/DishService.java
+++ restaraunt-service/src/main/java/com/restaraunt/dao/services/DishService.java
@@ -6,7 +6,9 @@ import java.util.stream.Collectors;
 
 import com.restaraunt.dao.entities.Dish;
 import com.restaraunt.dao.repositories.DishRepository;
+import com.restaraunt.exceptionhandler.ServiceException;
 import com.restaraunt.rest.dto.DishDto;
+import javax.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,18 +23,27 @@ public class DishService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Transactional
     public DishDto saveDish(DishDto dishDto){
         Dish dish = modelMapper.map(dishDto, Dish.class);
-        return modelMapper.map(dishRepository.save(dish), DishDto.class);
-    }
 
-    public Dish updateDish(Dish dish){
-        return dish;
+        try{
+            return modelMapper.map(dishRepository.save(dish), DishDto.class);
+        }catch (RuntimeException e){
+            log.error("Exception occurred during saving dish with id"+dish.getId(), e.getMessage());
+            throw new ServiceException("Exception occurred during saving dish "+dish.toString(), e.getCause());
+        }
+
     }
 
     public Optional<DishDto> getDishById(Long id){
         Optional<Dish> dish = dishRepository.findById(id);
-        return Optional.of(modelMapper.map(dish, DishDto.class));
+
+        if(dish.isPresent()){
+            return Optional.of(modelMapper.map(dish, DishDto.class));
+        }else {
+            throw new ServiceException("");
+        }
     }
 
     public List<DishDto> getAllDishes(){
@@ -42,6 +53,7 @@ public class DishService {
         return listOfDishes;
     }
 
+    @Transactional
     public void deleteDish(Long id){
         dishRepository.deleteById(id);
     }
---
+++ restaraunt-service/src/main/java/com/restaraunt/exceptionhandler/ServiceException.java
@@ -0,0 +1,14 @@
+package com.restaraunt.exceptionhandler;
+
+public class ServiceException extends RuntimeException{
+    public ServiceException() {
+    }
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
