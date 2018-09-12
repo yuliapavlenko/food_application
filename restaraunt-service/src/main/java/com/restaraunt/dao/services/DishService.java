cb911be672afc02b2e68325201f7e084c232ba86
yuliia.pavlenko <yuliia.pavlenko@ziftsolutions.com>
2018-09-12 20:24:11 +0300

    added methods in dish service

--- restaraunt-service/src/main/java/com/restaraunt/dao/services/DishService.java
+++ restaraunt-service/src/main/java/com/restaraunt/dao/services/DishService.java
@@ -1,60 +1,76 @@
 package com.restaraunt.dao.services;
 
+import javax.transaction.Transactional;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.restaraunt.dao.entities.Dish;
 import com.restaraunt.dao.repositories.DishRepository;
 import com.restaraunt.exceptionhandler.ServiceException;
 import com.restaraunt.rest.dto.DishDto;
-import javax.transaction.Transactional;
+
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class DishService {
     @Autowired
     private DishRepository dishRepository;
-
+    
     @Autowired
     private ModelMapper modelMapper;
-
+    
     @Transactional
-    public DishDto saveDish(DishDto dishDto){
+    public DishDto saveDish(DishDto dishDto) {
         Dish dish = modelMapper.map(dishDto, Dish.class);
-
-        try{
+        
+        try {
             return modelMapper.map(dishRepository.save(dish), DishDto.class);
-        }catch (RuntimeException e){
-            log.error("Exception occurred during saving dish with id"+dish.getId(), e.getMessage());
-            throw new ServiceException("Exception occurred during saving dish "+dish.toString(), e.getCause());
+        } catch (RuntimeException e) {
+            log.error("Exception occurred during saving of dish with id = " + dish.getId(), e.getMessage());
+            throw new ServiceException("Exception occurred during saving of dish " + dish.toString(), e.getCause());
         }
-
+        
     }
-
-    public Optional<DishDto> getDishById(Long id){
+    
+    public Optional<DishDto> getDishById(Long id) {
         Optional<Dish> dish = dishRepository.findById(id);
-
-        if(dish.isPresent()){
+        
+        if (dish.isPresent()) {
             return Optional.of(modelMapper.map(dish, DishDto.class));
-        }else {
-            throw new ServiceException("");
+        } else {
+            log.error("Exception occurred during search of dish " + dish.toString());
+            throw new ServiceException("Exception occurred during search of dish " + dish.toString());
         }
     }
-
-    public List<DishDto> getAllDishes(){
-        List<DishDto> listOfDishes = dishRepository.findAll().stream()
-                .map(dish -> modelMapper.map(dish, DishDto.class))
-                .collect(Collectors.toList());
+    
+    public List<DishDto> getAllDishes() {
+        List<DishDto> listOfDishes;
+        
+        try {
+            listOfDishes = dishRepository.findAll().stream()
+                    .map(dish -> modelMapper.map(dish, DishDto.class))
+                    .collect(Collectors.toList());
+        } catch (RuntimeException e) {
+            log.error("Exception occurred during search of all dishes", e.getMessage());
+            throw new ServiceException("Exception occurred during search of all dishes", e.getCause());
+        }
         return listOfDishes;
     }
-
+    
     @Transactional
-    public void deleteDish(Long id){
-        dishRepository.deleteById(id);
+    public void deleteDish(Long id) {
+        try {
+            dishRepository.deleteById(id);
+        } catch (RuntimeException e) {
+            log.error("Exception occurred during deleting of dish with id =" + id, e.getMessage());
+            throw new ServiceException("Exception occurred during deleting of dish with id = " + id, e.getCause());
+        }
     }
 }
