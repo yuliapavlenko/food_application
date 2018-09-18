216233293cc53901990817f1b613000ce13e6291
Yuliya Pavlenko <yuliapavlenko05@gmail.com>
2018-09-19 01:14:35 +0300

    added new methods for dish controller

--- restaraunt-service/src/main/java/com/restaraunt/rest/controllers/DishController.java
+++ restaraunt-service/src/main/java/com/restaraunt/rest/controllers/DishController.java
@@ -1,19 +1,23 @@
 package com.restaraunt.rest.controllers;
 
 import static com.restaraunt.constants.Constants.DISH_SERVICE_PATH;
+import static org.springframework.http.ResponseEntity.noContent;
 
 import java.util.List;
 import java.util.Optional;
 
+import com.restaraunt.dao.entities.Dish;
 import com.restaraunt.dao.services.DishService;
 import com.restaraunt.rest.dto.DishDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +47,15 @@ public class DishController {
     @PostMapping
     private ResponseEntity<DishDto> createDish(@RequestBody DishDto dish){
         return ResponseEntity.ok().body(dishService.saveDish(dish));
+    }
+
+    @PutMapping
+    private ResponseEntity<DishDto> updateDish(@RequestBody DishDto dish){
+        return ResponseEntity.ok().body(dishService.saveDish(dish));
+    }
+
+    @DeleteMapping
+    private ResponseEntity<?> deleteDish(@RequestBody DishDto dish){
+        return ResponseEntity.noContent().build();
     }
 }
