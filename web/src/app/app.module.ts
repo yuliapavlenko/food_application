811b2d1a78a899fc531f0760ec96b31e410b3718
yuliia.pavlenko <yuliia.pavlenko@ziftsolutions.com>
2018-09-25 20:07:51 +0300

    added footer, routing, 'not found' page

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {RouterModule, Routes} from "@angular/router";
+import {MainComponent} from './main/main.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'main', component: MainComponent},
+  {path: 'page-not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/page-not-found'
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
